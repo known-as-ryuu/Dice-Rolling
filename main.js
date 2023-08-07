639b6616920cc68f4673386a0c955aa77dd69125
Ryuu <103905200+known-as-ryuu@users.noreply.github.com>
2023-08-07 21:55:50 +0100

    v0.1

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 function rollDice(sides) {
     var resultElement = document.getElementById('result');
+    var rolledNumberElement = document.getElementById('rolledNumber');
+
     var result = Math.floor(Math.random() * sides) + 1;
-    resultElement.textContent = 'Rolagem: ' + result;
+    rolledNumberElement.textContent = 'Número Rolado: ' + result;
+
+    resultElement.style.display = 'block';
 }
