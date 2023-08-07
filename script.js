74425900c4a8f71e9a17e427e5c29a2818c4cbe2
Ryuu <103905200+known-as-ryuu@users.noreply.github.com>
2023-08-07 22:13:54 +0100

    v0.3

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-function rollDice(sides) {
-    var resultElement = document.getElementById('result');
-    var rolledNumberElement = document.getElementById('rolledNumber');
+function calculate() {
+    var inputNumber = parseInt(document.getElementById('inputNumber').value);
+    var calcResultElement = document.getElementById('calcResult');
 
-    var result = Math.floor(Math.random() * sides) + 1;
-    rolledNumberElement.textContent = 'Número Rolado: ' + result + ' (' + sides + ' faces)';
-
-    resultElement.style.display = 'block';
+    if (isNaN(inputNumber)) {
+        calcResultElement.textContent = 'Digite um número válido.';
+    } else {
+        var result = inputNumber * 2; // Exemplo de cálculo simples
+        calcResultElement.textContent = 'Resultado do cálculo: ' + result;
+    }
 }
