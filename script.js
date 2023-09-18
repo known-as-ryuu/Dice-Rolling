a8fbd95fa49749504413a4a2cbede5b7701900cc
Ryuu <103905200+known-as-ryuu@users.noreply.github.com>
2023-09-18 19:22:46 +0100

    v1.5.4
    - The state "BlackBackground" is now saved, so if you refresh the page it stays how you leaved it (black or not)

--- script.js
+++ script.js
@@ -396,10 +396,26 @@ function toggleBackground() {
   
   inputs.forEach(input => {
     input.classList.toggle('black-bg'); // Alterna a classe para cada input individualmente
+
   });
+  // Verifica se o primeiro input tem a clase 'black-bg' e armazena o estado no localStorage
+  const isBlackBackground = inputs[0].classList.contains('black-bg');
+  localStorage.setItem("valorBg", isBlackBackground);
 }
 
+// Verifica se há um valor armazenado no LocalStorage ao carregar a página
+const valorBg = localStorage.getItem("valorBg");
+
+if (valorBg === "true") {
+  // Se o valor for "true", significa que o background deve ser preto
+  const inputs = document.querySelectorAll('.d100Table');
+  inputs.forEach(input => {
+    input.classList.add('black-bg')
+  })
+}
 
 document.addEventListener("DOMContentLoaded", function () {
   document.body.classList.add("loaded");
 });
+
+
